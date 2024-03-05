3ba5e2d4d23bf5f0cd04546324c632f947e32840
AdrianMonemi <adrianmonemi6@gmail.com>
2024-03-05 13:12:43 +0100

    prompt the inputs

---
+++ src/main/java/org/Admon/UserInput.java
@@ -0,0 +1,26 @@
+package org.Admon;
+
+import java.util.Scanner;
+class UserInput {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double getDoubleInput(String prompt) {
+        System.out.print(prompt);
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Invalid input! Please enter a number.");
+            System.out.print(prompt);
+            scanner.next();
+        }
+        return scanner.nextDouble();
+    }
+
+    public static int getIntInput(String prompt) {
+        System.out.print(prompt);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input! Please enter a number between 1 and 4.");
+            System.out.print(prompt);
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+}
