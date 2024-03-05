950701862e40789062872e982cd292af33f3e02c
AdrianMonemi <adrianmonemi6@gmail.com>
2024-03-05 13:43:00 +0100

    fix Invalid-operation-loop in UserInput.getInt Input

--- src/main/java/org/Admon/UserInput.java
+++ src/main/java/org/Admon/UserInput.java
@@ -15,12 +15,20 @@ class UserInput {
     }
 
     public static int getIntInput(String prompt) {
-        System.out.print(prompt);
-        while (!scanner.hasNextInt()) {
-            System.out.println("Invalid input! Please enter a number between 1 and 4.");
+        int input;
+        do {
             System.out.print(prompt);
-            scanner.next();
-        }
-        return scanner.nextInt();
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input! Please enter a number.");
+                System.out.print(prompt);
+                scanner.next(); // Consume the invalid input
+            }
+            input = scanner.nextInt();
+            if (input < 1 || input > 4) {
+                System.out.println("Invalid input! Please enter a number between 1 and 4.");
+            }
+        } while (input < 1 || input > 4);
+        return input;
     }
+
 }
